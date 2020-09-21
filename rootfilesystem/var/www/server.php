b672a79e5599607ceb78aa93a8c2273d7f53a3ed
JerryHopper <hopper.jerry@gmail.com>
2020-09-21 11:06:01 +0200

    Update server.php

--- rootfilesystem/var/www/server.php
+++ rootfilesystem/var/www/server.php
@@ -1,20 +1,37 @@
 #!/usr/bin/env php
 <?php
 
-declare(strict_types=1);
+use Swoole\WebSocket\Server;
+use Swoole\Http\Request;
+use Swoole\WebSocket\Frame;
 
-$http = new Swoole\Http\Server("0.0.0.0", 9501);
-$http->on(
-    "request",
-    function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
-        $response->end(
-            <<<EOT
-                <pre>
-                In this example we have built-in script "/var/www/server.php" in the Docker image overwritten with this
-                one.
-                </pre>
-            EOT
-        );
-    }
-);
-$http->start();
+
+echo "XSTARTX\n";
+
+$server = new Server("0.0.0.0", 9501 ,SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
+
+
+
+
+$server->on("start", function (Server $server) {
+    echo "Swoole WebSocket Server is started at http://127.0.0.1:9501\n";
+});
+
+$server->on('open', function(Server $server, Swoole\Http\Request $request) {
+    echo "connection open: {$request->fd}\n";
+    $server->tick(1000, function() use ($server, $request) {
+        $server->push($request->fd, json_encode(["hello", time()]));
+    });
+});
+
+$server->on('message', function(Server $server, Frame $frame) {
+    echo "received message: {$frame->data}\n";
+    $server->push($frame->fd, json_encode(["hello", time()]));
+});
+
+$server->on('close', function(Server $server, int $fd) {
+    echo "connection close: {$fd}\n";
+});
+
+$server->start();
+
